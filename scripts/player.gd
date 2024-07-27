efca35c1ee767420194e63987ca945c860e38de7
Lu <luxcc2002@gmail.com>
2024-07-27 02:16:38 -0400

    feat: Added hands to the character
    Character now has two nodes that represent the position of each of their hands

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,13 +4,18 @@ extends CharacterBody3D
 const SPEED = 10
 const JUMP_VELOCITY = 7
 var sensibility = 0.7
-var max_camera_angle = 70
-var min_camera_angle = -70
+var max_camera_angle = 90
+var min_camera_angle = -45
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var camera_anchor := $CameraAnchor
-@onready var camera := $CameraAnchor/Camera3D
+@onready var camera_anchor_y := $CameraAnchorY
+@onready var camera_anchor_x = $CameraAnchorY/CameraAnchorX
+@onready var camera := $CameraAnchorY/CameraAnchorX/Camera3D
+@onready var r_hand = $CameraAnchorY/CameraAnchorX/RHand
+@onready var l_hand = $CameraAnchorY/CameraAnchorX/LHand
+
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -19,24 +24,22 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			camera_anchor.rotate_y(-event.relative.x * 0.005 * sensibility)
-			camera.rotate_x(-event.relative.y * 0.005 * sensibility)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(min_camera_angle), deg_to_rad(max_camera_angle))
+			camera_anchor_y.rotate_y(-event.relative.x * 0.005 * sensibility)
+			camera_anchor_x.rotate_x(-event.relative.y * 0.005 * sensibility)
+			camera_anchor_x.rotation.x = clamp(camera_anchor_x.rotation.x, deg_to_rad(min_camera_angle), deg_to_rad(max_camera_angle))
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction = (camera_anchor.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "forward", "backwards")
+	var direction = (camera_anchor_y.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
